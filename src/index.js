bad4f8c9c04c8ad29be5e9bb855dd67bf945c086
sten00777 <www.ep965@gmail.com>
2021-03-14 17:45:40 +0300

    feat: add code to index.js

--- src/index.js
+++ src/index.js
@@ -2,5 +2,27 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+ // return matrix.flat().sort(function compareNumbers(a, b) {
+   // return a - b;
+  //});
+
+  /*if (matrix){
+    return matrix.flat().sort(function compareNumbers(a, b) {
+      return a - b;
+    });
+  } else if (matrix === undefined){
+    return [];
+  } return [];*/
+let res = [];
+  if (matrix){
+    for (let i = 0; i < matrix.length; i++) {
+      if (i % 2 !== 0) {
+        matrix[i].reverse();
+      }
+    }
+   return matrix.flat(Infinity);
+    } else if (matrix === undefined){
+    return [];
+  } return [];
+  
 }
